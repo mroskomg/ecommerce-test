a820b3e004ceaec86bbff33612ba32e12aa9ba26
Michael Mrosko <mroskomg@gmail.com>
2020-02-14 14:17:21 -0500

    added action/reducer to add cart drop down.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user-actions";
+// import { setCartVisibility } from "./redux/cart/cart-actions";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -36,7 +37,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <Header setCartVisibility={this.props.setCartVisibility} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
@@ -57,8 +58,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = ({ user, cart }) => ({
+  currentUser: user.currentUser,
+  cartVisibility: cart.hidden
 });
 
 const mapDispatchToProps = dispatch => ({
