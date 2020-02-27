1e603772cba2a57ab4e7ce569aeddbdca1423c85
Michael Mrosko <mroskomg@gmail.com>
2020-02-27 14:45:36 -0500

    added stripe integration and updated favicon and app title.

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_2N1ol4zLdWpvQo59qUzRSxnc00K2lMQ9di";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Test Clothing Co"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
